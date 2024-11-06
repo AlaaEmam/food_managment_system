73e15c60fbee27b9079d770f714bc792f01b6345
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2024-11-06 13:53:06 +0200

    routing

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AuthLayout from './modules/shared/components/AuthLayout/AuthLayout';
+import Login from './modules/authentication/components/Login/Login';
+import Registration from './modules/authentication/components/Registration/Registration';
+import ForgetPass from './modules/authentication/components/ForgetPass/ForgetPass';
+import ResetPass from './modules/authentication/components/ResetPass/ResetPass';
+import NotFound from './modules/shared/components/NotFound/NotFound';
+import MasterLayout from './modules/shared/components/MasterLayout/MasterLayout';
+import Dashboard from './modules/dashboard/components/Dashboard/Dashboard';
+import RecipesList from './modules/recipes/components/RecipesList/RecipesList';
+import RecipeData from './modules/recipes/components/RecipeData/RecipeData';
+import CategoriesList from './modules/categories/components/CategoriesList/CategoriesList';
+import CategoryData from './modules/categories/components/CategoryData/CategoryData';
+import UserList from './modules/users/components/UserList/UserList';
+
+
+function App() {
+ 
+  const routes = createBrowserRouter([
+    {
+      path: '',
+      element: <AuthLayout/>, 
+      errorElement: <NotFound/>,
+      children:[
+        {index: true ,element: <Login/>},
+        {path: 'login' ,element: <Login/>},
+        {path: 'register' ,element:<Registration/>},
+        {path: 'forget-pass' ,element:<ForgetPass/>},
+        {path: 'reset-pass' ,element:<ResetPass/>},
+      ]
+    },
+    {
+      path: 'Dashboard',
+      element: <MasterLayout/>, 
+      errorElement: <NotFound/>,
+      children:[
+        {index: true ,element: <Dashboard/>},
+        {path: 'recipes' ,element: <RecipesList/>},
+        {path: 'recipe-data' ,element:<RecipeData/>},
+        {path: 'categories' ,element:<CategoriesList/>},
+        {path: 'category-data' ,element:<CategoryData/>},
+        {path: 'users' ,element:<UserList/>},
+      ]
+    }
+  ])
+
+  return (
+    <>
+    <RouterProvider router={routes}></RouterProvider>
+    </>
+  )
+}
+
+export default App

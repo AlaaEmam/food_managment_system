ec96da106a9365fad8aa9ac42317b3c149ee23aa
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2024-11-08 01:29:08 +0200

    api login

--- src/modules/authentication/components/Login/Login.jsx
+++ src/modules/authentication/components/Login/Login.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../../../assets/logo.svg'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import axios from 'axios';
 
 export default function Login() {
+
+  let { 
+    register, //collect data and put data in email and password ---> register('email') 
+    handleSubmit, //Handle when submit 
+    formState: { errors } // get error massage
+  } = useForm();
+  const onSubmit = async (data) => {
+    try{
+      let response = await axios.post('https://upskilling-egypt.com:3006/api/v1/Users/Login', data);
+      console.log(response);
+    navigate('/dashboard');
+    }catch(error){
+      console.log(error);
+    }
+      // show data after form submission
+      // console.log(data);
+  };
+
+  // Show and Hide Password
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+};
+
   return (
     <div className='auth-container'>
       <div className="container-fluid ">
@@ -16,37 +43,74 @@ export default function Login() {
                 <h3>Login</h3>
                 <p className='text-muted'>Welcome Back! Please enter your details</p>
               </div>
-              <form action="">
 
-                <div className="input-group mb-3 rounded-2 ">
+              <form onSubmit={handleSubmit(onSubmit)} className="col-lg-12 col-sm-12 " action="">
+                {/* INPUT EMAIL */}
+                <div className="input-group mb-1 rounded-2 ">
+                  <div className="left">
                     <div className="input-group-prepend">
-                      <span className="icon-input" id="basic-addon1">
-                      <i className="fa-regular fa-envelope"></i>
-                        </span>
-                    </div>
-                    <input 
-                    type="text" 
-                    className="input-style bg-transparent" 
-                    placeholder="Enter your E-mail" 
-                    aria-label="email" 
-                    aria-describedby="basic-addon1"/>
+                        <span className="icon-input" id="basic-addon1">
+                        <i className="fa-regular fa-envelope"></i>
+                          </span>
+                      </div>
+                        <input 
+                        type="text" 
+                        className="input-style bg-transparent"
+                        placeholder="Enter your E-mail" 
+                        aria-label="email" 
+                        aria-describedby="basic-addon1" 
+                        {...register('email', 
+                          {
+                            required: 'Email is required. Please enter your email address.',
+                            pattern: {
+                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                message: 'Please enter a valid email address like this (user@example.com)'
+                            }
+                        })}
+                        />
+                  </div>
                 </div>
-                <div className="input-group mb-3 rounded-2">
-                    <div className="input-group-prepend">
-                      <span className="icon-input" id="basic-addon1">
-                      <i className="fa-solid fa-lock"></i>
+                <div className='mb-3'>                
+                  {errors.email && <span className='text-danger'>{errors.email.message}</span>}
+                </div>
+
+                {/* INPUT PASSWORD */}
+                <div className="input-group mb-1 rounded-2">
+                    <div className='left'>
+                        <div className="input-group-prepend">
+                        <span className="icon-input" id="basic-addon1">
+                        <i className="fa-solid fa-lock"></i>
+                          </span>
+                      </div>
+                      <input 
+                      type={ showPassword ? 'text' : 'password'}
+                      className="input-style bg-transparent" 
+                      placeholder="Enter your password" 
+                      aria-label="password" 
+                      aria-describedby="basic-addon1"
+                      {...register('password' ,
+                        {
+                          required: 'Password is required. Please enter your Password.',
+                          minLength: {
+                            value: 6,
+                            message: 'Password must be at least 6 characters long'
+                        }}
+                      )}
+                      />
+                    </div>
+                     <div className="input-group-prepend ">
+                        <span className="icon-showpass" onClick={togglePasswordVisibility} style={{ cursor: 'pointer' }}>
+                            {showPassword ? <i className="fa-solid fa-eye-slash"></i> : <i className="fa-solid fa-eye"></i>}
                         </span>
                     </div>
-                    <input 
-                    type="password" 
-                    className="input-style bg-transparent" 
-                    placeholder="Enter your password" 
-                    aria-label="password" 
-                    aria-describedby="basic-addon1"/>
+                </div>
+               
+                <div className='mb-3'>                
+                  {errors.password && <span className='text-danger'>{errors.password.message}</span>}
                 </div>
                   
                   <div className="links d-flex justify-content-between mb-3">
-                    <Link to='register' className='text-dark text-monospace text-decoration-none '>Register Now?</Link>
+                    <Link to='register' className='text-dark text-monospace text-decoration-none font-weight-bold'>Register Now?</Link>
                     <Link to='forget-pass' className='text-success text-decoration-none'> Forgot Password?</Link>
                   </div>
                   <button className='btn btn-success w-100 p-2 mt-4 mb-2 tex'> Login</button>

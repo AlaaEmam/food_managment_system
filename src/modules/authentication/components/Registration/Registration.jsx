bb61606aab42520aa64010a504ca92b5f66797ef
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2024-11-09 00:33:42 +0200

    register page

--- src/modules/authentication/components/Registration/Registration.jsx
+++ src/modules/authentication/components/Registration/Registration.jsx
@@ -1,180 +1,200 @@
-// import React, { useState } from 'react'
-// import Logo from '../../../../assets/logo.svg'
-// import { Link, useNavigate } from 'react-router-dom'
-// import { useForm } from 'react-hook-form'
-// import axios from 'axios';
-// import { toast } from 'react-toastify';
+import Logo from '../../../../assets/logo.svg';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useState } from 'react';
 
- export default function Registration() {
+export default function ForgetPass() {
+  const navigate = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [showPassword, setShowPassword] = useState(false);
 
-//   const navigate = useNavigate(); 
+  const onSubmit = async (data) => {
+    try {
+      let response = await axios.post('https://upskilling-egypt.com:3006/api/v1/Users/Register', data);
+      navigate('/login');
+      toast.success(response.data.message);
+    } catch (error) {
+      toast.error(error.response?.data.message || 'An error occurred');
+    }
+  };
 
-//   let { 
-//     register, //collect data and put data in email and password ---> register('email') 
-//     handleSubmit, //Handle when submit 
-//     formState: { errors } // get error massage
-//   } = useForm();
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-//   const onSubmit = async (data) => {
-//     try{
-//       let response = await axios.post('https://upskilling-egypt.com:3006/api/v1/Users/Register', data);
-//       navigate('/dashboard'); 
-//       toast.success("register Successfly")
-//       console.log(response);
-      
-//     }catch(error){
-//       console.log(error);
-//       toast.error(error.response?.data.message || 'An error occurred'); // Handle errors
-//     }
-//       // show data after form submission
-//       // console.log(data);
-//   };
+  return (
+    <>
+      <div className='auth-container'>
+        <div className="container-fluid">
+          <div className="row vh-100 justify-content-center align-content-center">
+            <div className="col-md-6 col-sm-6 bg-white rounded-2 px-5 py-3">
+              <div className="logo-container text-center">
+                <img className='img-fluid w-55 mt-3' src={Logo} alt="" />
+              </div>
+              <div className="title my-4">
+                <h3>Register</h3>
+                <p className='text-muted'>Welcome Back! Please enter your details.</p>
+              </div>
 
-//   // Show and Hide Password
-//   const [showPassword, setShowPassword] = useState(false);
+              <form onSubmit={handleSubmit(onSubmit)} className="col-lg-12 col-sm-12">
+                <div className="row mb-3 g-3"> 
+                  {/* First Column */}
+                  <div className='col-md-12 col-lg-6'>
+                    {/* INPUT USER NAME */}
+                    <div className="input-group mb-1 rounded-2">
+                      <span className="input-group-text">
+                        <i className="fa-regular fa-user"></i>
+                      </span>
+                      <input 
+                        type="text" 
+                        className="input-style bg-transparent"
+                        placeholder="Enter your Username" 
+                        aria-label="name" 
+                        {...register('userName', {
+                          required: 'Your Name is required. Please enter your Name.',
+                          pattern: {
+                            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z0-9]*\d$/,
+                            message: 'Username must contain characters and end with numbers without spaces.'
+                          }
+                        })}
+                      />
+                    </div>
+                    <div className='mb-3'>
+                      {errors.userName && <span className='text-danger'>{errors.userName.message}</span>}
+                    </div>
 
-//   const togglePasswordVisibility = () => {
-//     setShowPassword(!showPassword);
-// };
+                    {/* INPUT COUNTRY */}
+                    <div className="input-group mb-1 rounded-2">
+                      <span className="input-group-text">
+                        <i className="fa-solid fa-globe"></i>
+                      </span>
+                      <input 
+                        type="text" 
+                        className="input-style bg-transparent"
+                        placeholder="Country" 
+                        aria-label="Country" 
+                        {...register('country')}
+                      />
+                    </div>
+                    <div className='mb-3'>
+                      {errors.country && <span className='text-danger'>{errors.country.message}</span>}
+                    </div>
 
+                    {/* INPUT NEW PASSWORD */}
+                    <div className="input-group mb-1 rounded-2">
+                      <span className="input-group-text">
+                        <i className="fa-solid fa-lock"></i>
+                      </span>
+                      <input 
+                        type={showPassword ? 'text' : 'password'}
+                        className="input-style"
+                        placeholder="New Password"
+                        aria-label="password" 
+                        {...register('password', {
+                          required: 'Password is required.',
+                          minLength: {
+                            value: 6,
+                            message: 'Password must be at least 6 characters long'
+                          }
+                        })}
+                      />
+                      <span 
+                        className="input-group-text icon-showpass" 
+                        onClick={togglePasswordVisibility} 
+                        style={{ cursor: 'pointer' }}>
+                        {showPassword ? <i className="fa-solid fa-eye"></i> : <i className="fa-solid fa-eye-slash"></i>}
+                      </span>
+                    </div>
+                    <div className='mb-3'>
+                      {errors.password && <span className='text-danger'>{errors.password.message}</span>}
+                    </div>
+                  </div>
 
-//   return (
-//     <>
-//     <div className='auth-container'>
-//       <div className="container-fluid ">
-//       <div className="row vh-100">
+                  {/* Second Column */}
+                  <div className='col-md-12 col-lg-6'>
+                    {/* INPUT EMAIL */}
+                    <div className="input-group mb-1 rounded-2">
+                      <span className="input-group-text">
+                        <i className="fa-regular fa-envelope"></i>
+                      </span>
+                      <input 
+                        type="text" 
+                        className="input-style bg-transparent"
+                        placeholder="Enter your E-mail" 
+                        aria-label="email" 
+                        {...register('email', {
+                          required: 'Email is required. Please enter your email address.',
+                          pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: 'Please enter a valid email address like this (user@example.com)'
+                          }
+                        })}
+                      />
+                    </div>
+                    <div className='mb-3'>
+                      {errors.email && <span className='text-danger'>{errors.email.message}</span>}
+                    </div>
 
-//           <div className="logo-container text-center">
-//               <img className='img-fluid w-55 mt-3' src={Logo} alt="" />            
-//           </div>
-//           <div className="title my-4">
-//             <h3>Register</h3>
-//             <p className='text-muted'>Welcome Back! Please enter your details</p>
-//           </div>
+                    {/* INPUT PHONE NUMBER */}
+                    <div className="input-group mb-1 rounded-2">
+                      <span className="input-group-text">
+                        <i className="fa-solid fa-phone"></i>
+                      </span>
+                      <input 
+                        type="text" 
+                        className="input-style bg-transparent"
+                        placeholder="Phone Number" 
+                        aria-label="Phone Number" 
+                        {...register('phoneNumber', {
+                          pattern: {
+                            value: /^01[0-9]{9}$/,
+                            message: 'Phone number must start with 01 and contain 11 digits.'
+                          }
+                        })}
+                      />
+                    </div>
+                    <div className='mb-3'>
+                      {errors.phoneNumber && <span className='text-danger'>{errors.phoneNumber.message}</span>}
+                    </div>
 
-//            <form action="">
-//            <div className="col">Column</div>
-//             <div className="col">Column</div>
-//             <div className="w-100"></div>
-//             <div className="col">Column</div>
-//             <div className="col">Column</div>
-//             <div className="w-100"></div>
-//             <div className="col">Column</div>
-//             <div className="col">Column</div>
-//            </form>
-//         </div>
-//       </div>
-//     </div>
-//     </>
-//   )
+                    {/* INPUT CONFIRM NEW PASSWORD */}
+                    <div className="input-group mb-1 rounded-2">
+                      <span className="input-group-text">
+                        <i className="fa-solid fa-lock"></i>
+                      </span>
+                      <input 
+                        type={showPassword ? 'text' : 'password'}
+                        className="input-style"
+                        placeholder="Confirm New Password"
+                        aria-label="password" 
+                        {...register('confirmPassword', {
+                          required: 'Confirm Password is required.',
+                          minLength: {
+                            value: 6,
+                            message: 'Confirm Password must be at least 6 characters long'
+                          }
+                        })}
+                      />
+                      <span 
+                        className="input-group-text icon-showpass" 
+                        onClick={togglePasswordVisibility} 
+                        style={{ cursor: 'pointer' }}>
+                        {showPassword ? <i className="fa-solid fa-eye"></i> : <i className="fa-solid fa-eye-slash"></i>}
+                      </span>
+                    </div>
+                    <div className='mb-3'>
+                      {errors.confirmPassword && <span className='text-danger'>{errors.confirmPassword.message}</span>}
+                    </div>
+                  </div>
+                </div>
+                <button className='btn btn-success w-100 p-2 mt-4 mb-2'>Register</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
-// <div className="row vh-100 justify-content-center align-content-center">
-// <div className="col-md-6 col-sm-6 bg-white rounded-2 px-5 py-3">
-//   <div> 
-//     <div className="logo-container text-center">
-//         <img className='img-fluid w-55 mt-3' src={Logo} alt="" />            
-//     </div>
-//     <div className="title my-4">
-//       <h3>Register</h3>
-//       <p className='text-muted'>Welcome Back! Please enter your details</p>
-//     </div>
-
-//     <form onSubmit={handleSubmit(onSubmit)} className="col-lg-12 col-sm-12 " action="">
-      
-//           {/* INPUT UserName */}
-//           <div className="input-group mb-1 rounded-2">
-//           <div className="input-group-prepend">
-//               <span className="icon-input" id="basic-addon1">
-//               <i className="fa-regular fa-envelope"></i>
-//                 </span>
-//             </div>
-//               <input 
-//               type="text" 
-//               className="input-style bg-transparent"
-//               placeholder="Enter your User Name" 
-//               aria-label="name" 
-//               aria-describedby="basic-addon1" 
-//               {...register('userName', 
-//                 {
-//                   required: ' Please enter your user Name',
-//               })}
-//               />
-//       </div>
-//       <div className='mb-3'>                
-//         {errors.userName && <span className='text-danger'>{errors.userName.message}</span>}
-//       </div>
-      
-//       {/* INPUT EMAIL */}
-//       <div className="input-group mb-1 rounded-2 ">
-//         <div className="left">
-//           <div className="input-group-prepend">
-//               <span className="icon-input" id="basic-addon1">
-//               <i className="fa-regular fa-envelope"></i>
-//                 </span>
-//             </div>
-//               <input 
-//               type="text" 
-//               className="input-style bg-transparent"
-//               placeholder="Enter your E-mail" 
-//               aria-label="email" 
-//               aria-describedby="basic-addon1" 
-//               {...register('email', 
-//                 {
-//                   required: 'Email is required. Please enter your email address.',
-//                   pattern: {
-//                       value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-//                       message: 'Please enter a valid email address like this (user@example.com)'
-//                   }
-//               })}
-//               />
-//         </div>
-//       </div>
-//       <div className='mb-3'>                
-//         {errors.email && <span className='text-danger'>{errors.email.message}</span>}
-//       </div>
-
-//       {/* INPUT PASSWORD */}
-//       <div className="input-group mb-1 rounded-2">
-//           <div className='left'>
-//               <div className="input-group-prepend">
-//               <span className="icon-input" id="basic-addon1">
-//               <i className="fa-solid fa-lock"></i>
-//                 </span>
-//             </div>
-//             <input 
-//             type={ showPassword ? 'text' : 'password'}
-//             className="input-style bg-transparent" 
-//             placeholder="Enter your password" 
-//             aria-label="password" 
-//             aria-describedby="basic-addon1"
-//             {...register('password' ,
-//               {
-//                 required: 'Password is required. Please enter your Password.',
-//                 minLength: {
-//                   value: 6,
-//                   message: 'Password must be at least 6 characters long'
-//               }}
-//             )}
-//             />
-//           </div>
-//            <div className="input-group-prepend ">
-//               <span className="icon-showpass" onClick={togglePasswordVisibility} style={{ cursor: 'pointer' }}>
-//                   {showPassword ? <i className="fa-solid fa-eye"></i> : <i className="fa-solid fa-eye-slash"></i>}
-//               </span>
-//           </div>
-//       </div>
-     
-//       <div className='mb-3'>                
-//         {errors.password && <span className='text-danger'>{errors.password.message}</span>}
-//       </div>
-        
-//         <div className="links d-flex justify-content-between mb-3">
-//           <Link to='register' className='text-dark text-monospace text-decoration-none font-weight-bold'>Register Now?</Link>
-//           <Link to='forget-pass' className='text-success text-decoration-none'> Forgot Password?</Link>
-//         </div>
-//         <button className='btn btn-success w-100 p-2 mt-4 mb-2 tex'> Login</button>
-//     </form>
-//   </div>
-// </div>
-// </div>

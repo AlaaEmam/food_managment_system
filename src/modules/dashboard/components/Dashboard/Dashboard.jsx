8657d700a593b4b511765aa38575b78ed0a133fb
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2024-11-14 02:40:52 +0200

    finish dashboard

--- src/modules/dashboard/components/Dashboard/Dashboard.jsx
+++ src/modules/dashboard/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../../shared/components/Header/Header';
 import avatar from '../../../../assets/header-avatar.png';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard({ loginData }) {
   return (
@@ -12,6 +13,19 @@ export default function Dashboard({ loginData }) {
         img={<img src={avatar} alt="User Avatar" />}
        
       />
+
+  <div className='dashboard-container p-5 h-25 d-flex justify-content-between position-relative align-items-center'>
+      <div className='caption '>
+        <h3 className='fw-bold'>Fill the <span className='fw-normal text-success'> Recipes </span> ! </h3>
+        <p className='fw-light'>you can now fill the meals easily using the table and form , click here and sill it with the table !</p>
+      </div>
+      <div className='end-0'>
+      <Link to='/dashboard/recipes' >
+      <button className=' btn btn-success  p-2 mt-4 mb-2 tex'>Fill Recipes<i className="fa-solid fa-arrow-right px-2"></i> </button>
+      </Link>
+      
+      </div>
+    </div>
     </>
   );
 }
